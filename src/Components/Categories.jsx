1fcd4d7670eb50e353224e389909651802e32f1f
kponky <ikponkedet@gmail.com>
2024-07-07 17:52:47 +0100

    header 2-styled

--- src/Components/Categories.jsx
+++ src/Components/Categories.jsx
@@ -1,19 +1,19 @@
+import React from "react";
+
 import { faWindows } from "@fortawesome/free-brands-svg-icons";
 import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
-import "../Styles/allCategories.css"
 
-const AllCategories = () => {
+import "../Styles/allCategories.css";
+
+const Categories = () => {
   return (
     <div className="categories-container">
       <FontAwesomeIcon icon={faWindows} />
       <h6>All Categories</h6>
       <FontAwesomeIcon icon={faAngleDown} />
-
-       
     </div>
   );
 };
 
-export default AllCategories;
+export default Categories;
--- src/Components/header2.jsx
+++ src/Components/header2.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import logo from "../assets/Black-Brand.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
+  faAngleDown,
   faCartShopping,
   faSearch,
   faUser,
@@ -15,48 +16,68 @@ import MensWear from "./MensWear";
 import Brands from "./Brands";
 import AboutUs from "./AboutUs";
 import ContactUs from "./ContactUs";
+import "../Styles/header2.css";
+import Categories from "./Categories";
 
 const Header2 = () => {
   return (
     <div className="header2-container">
       <div className="header2-a">
         <div className="logo">
-          <img src={logo} alt="fashionest" />
+          <h1>
+            F<span>ashionnest</span>
+          </h1>
         </div>
 
-        <div className="search-input">
-          <input
-            type="text"
-            name="search item"
-            placeholder="Search for your items..."
-          />
-          <FontAwesomeIcon icon={faSearch} />
-        </div>
+        <div className="leftside">
+          <div className="search-input">
+            <input
+              type="text"
+              name="search item"
+              placeholder="Search for your items..."
+            />
+            <button>
+              {" "}
+              <FontAwesomeIcon icon={faSearch} className="icon" />
+            </button>
+          </div>
 
-        <div className="login-reg">
-          <a href="#">
-            <FontAwesomeIcon icon={faUser} />
-            <span>Login/Registration</span>
-          </a>
-        </div>
+          <div className="login-reg">
+            <a href="#">
+              <FontAwesomeIcon icon={faUser} />
+              <span>Login/Registration</span>
+            </a>
+          </div>
 
-        <div className="cart">
-          <a href="#">
-            <FontAwesomeIcon icon={faCartShopping} />
-            <span>2</span>
-            <p>Cart</p>
-          </a>
+          <div className="cart">
+            <a href="#">
+              <FontAwesomeIcon icon={faCartShopping} className="icon" />
+              <span className="badge">2</span>
+              <p>Cart</p>
+            </a>
+          </div>
         </div>
       </div>
 
       <div className="header2-b">
-        <AllCategories />
-        <HomeComp />
-        <Collection />
-        <NewArrivals />
-        <WomanWear />
-        <MensWear />
-        <Brands />
+        <Categories />
+        <div className="headerList_items">
+          <ul className="list-items">
+            <li className="listed">Home</li>
+            <li className="listed">
+              Collection <FontAwesomeIcon icon={faAngleDown} />
+            </li>
+            <li className="listed">New Arrivals</li>
+            <li className="listed">Women's Wears</li>
+            <li className="listed">Men's Wears</li>
+            <li className="listed">Brands</li>
+            <div className="buttons">
+            <button>About us</button>
+            <button>Contact us</button>
+            
+            </div>
+          </ul>
+        </div>
 
         <div className="about-contactus">
           <button>
@@ -72,3 +93,9 @@ const Header2 = () => {
 };
 
 export default Header2;
+// <HomeComp />
+//         <Collection />
+//         <NewArrivals />
+//         <WomanWear />
+//         <MensWear />
+//         <Brands />
