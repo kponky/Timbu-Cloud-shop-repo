44ea449a5ae19f864700ade256b6f0799f661466
kponky <ikponkedet@gmail.com>
2024-07-09 19:27:15 +0100

    refix

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,20 +1,18 @@
-import React from 'react'
-import Footer from '../Components/Footer'
-import Header1 from '../Components/Header1';
-import HeroSection from '../Components/HeroSection'
-import Header2 from '../Components/Header2';
+import React from "react";
+import Footer from "../Components/Footer";
+import Header1 from "../Components/Header1";
+import HeroSection from "../Components/HeroSection";
+import Header2 from "../Components/Header2";
 
 const Home = () => {
   return (
     <div>
-    <Header1/>
-    <Header2/>
-  
-    <HeroSection/>
-    <Footer/>
-
+      <Header1 />
+      <Header2 />
+      <HeroSection />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
