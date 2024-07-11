78719bfc69805475b76616ab13bfdb11739b3d01
kponky <ikponkedet@gmail.com>
2024-07-11 23:26:29 +0100

    refix

--- src/Components/CardHeader.jsx
+++ src/Components/CardHeader.jsx
@@ -2,13 +2,20 @@ import React from "react";
 import logo from "../assets/Black-Brand.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
-import '../Styles/cardHeader.css'
+import "../Styles/cardHeader.css";
+import { Link } from "react-router-dom";
 
 const CardHeader = () => {
   return (
     <div className="cardheader-wrapper">
-      <img src={logo} alt="" />
-      <FontAwesomeIcon icon={faTimes} className="icon" />
+      <Link to="/">
+        <img src={logo} alt="" />
+      </Link>
+
+      <Link to="/checkout">
+        
+        <FontAwesomeIcon icon={faTimes} className="icon" />
+      </Link>
     </div>
   );
 };
